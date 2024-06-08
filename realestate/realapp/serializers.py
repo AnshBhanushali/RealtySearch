3cdfce78ff9d860c6c0a96bc0b14dba4e815dcb0
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-08 00:37:13 -0400

    Installation

--- realestate/realapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class PropertySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Property
-        fields = ('property_id', 'property_name')
