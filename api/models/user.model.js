51439d0bfbb7fe4fec8051857bea13dfb13f6393
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-09 23:10:06 -0400

    creat signup api

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,10 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
 
-}, { timestamps: true });
+}, 
+{ timestamps: true }
+
+);
 
 const User = mongoose.model('User', userSchema);
 
